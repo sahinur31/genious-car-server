4d9f06291184d5ef338bb5d06e0c359a7a5094b7
sahinur31 <sahinur4841@gmail.com>
2021-10-27 11:05:04 +0600

    any

--- index.js
+++ index.js
@@ -47,6 +47,7 @@ async function run() {
             res.json(service);
         })
         // DELETE API
+        // deleteedes
         app.delete('/services/:id', async (req, res) => {
             const id = req.params.id;
             const query = { _id: ObjectId(id) };
